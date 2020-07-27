0df24080f3a3c2f76f0a32936cc3d46275256352
Fabrice Payet <hello@fabricepayet.fr>
2020-07-27 17:27:22 +0400

    add mock search results data

--- controllers/search.js
+++ controllers/search.js
@@ -1,6 +1,29 @@
+import { v4 as uuidv4 } from "uuid";
+
+const fakeResults = [
+  {
+    _id: uuidv4(),
+    type: "client",
+    context: "Client",
+    name: "Fabrice Payet",
+  },
+  {
+    _id: uuidv4(),
+    type: "commercial",
+    context: "Commercial",
+    name: "Fabrice Ranguin",
+  },
+  {
+    _id: uuidv4(),
+    type: "document",
+    context: "Documents",
+    name: "Les secrets de l'investisseur immo",
+  },
+];
+
 export async function searchTerm(req, res) {
   try {
-    return res.json({ success: true, data: [] });
+    return res.json({ success: true, data: fakeResults });
   } catch (e) {
     return res.status(500).json({ success: false });
   }
