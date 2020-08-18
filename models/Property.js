ef8537e962299a1fe704e3f852de9bb474e65dad
Fabrice Payet <hello@fabricepayet.fr>
2020-08-18 16:16:54 +0400

    Complete property model

--- models/Property.js
+++ models/Property.js
@@ -1,7 +1,33 @@
 import mongoose from "mongoose";
 
-var schema = new mongoose.Schema(
+const AddressSchema = new mongoose.Schema({
+  city: {
+    type: String,
+  },
+  zipcode: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+});
+
+const schema = new mongoose.Schema(
   {
+    name: {
+      type: String,
+    },
+    ref: {
+      type: String,
+      required: false,
+    },
+    description: {
+      type: String,
+    },
+    fullAddress: {
+      type: AddressSchema,
+      required: false,
+    },
     type: {
       type: String,
       enum: [
@@ -15,6 +41,26 @@ var schema = new mongoose.Schema(
     },
     area: {
       type: Number,
+    },
+    yearOfConstruction: {
+      type: Number,
+      required: false,
+    },
+    landArea: {
+      type: Number,
+      required: false,
+    },
+    livingArea: {
+      type: Number,
+      required: false,
+    },
+    salesPrice: {
+      type: Number,
+      required: false,
+    },
+    varangueArea: {
+      type: Number,
+      required: false,
     },
     photos: {
       type: Array,
@@ -37,5 +83,16 @@ var schema = new mongoose.Schema(
     collection: "properties",
   }
 );
+
+schema.pre("save", async function (next) {
+  try {
+    const propertiesCount = await mongoose.models["Document"].countDocuments();
+    const refTemps = `00000000${propertiesCount}`;
+    this.ref = `${refTemps.substring(propertiesCount.toString().length)}`;
+    next();
+  } catch (e) {
+    next(e);
+  }
+});
 
 export default mongoose.model("Property", schema);
