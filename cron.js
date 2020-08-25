2909944b44ee3db9a742787c1b58cc39a9ccc551
Fabrice Payet <hello@fabricepayet.fr>
2020-08-25 15:13:14 +0400

    Fix cron relance

--- cron.js
+++ cron.js
@@ -3,9 +3,9 @@ import Project from "./models/Project";
 import moment from "moment";
 import { sendClientReminder } from "./lib/email";
 
-cron.schedule("0 0 * * * *", () => {
+cron.schedule("0 0 * * * *", async () => {
   // Project createdAt between 1 and 2 hours ago
-  const projects = Project.find({
+  const projects = await Project.find({
     status: "missing_information",
     createdAt: {
       $gte: moment().subtract(2, "hours"),
