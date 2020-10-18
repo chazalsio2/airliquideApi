84d3dc1199e4527e49e5fa5885a6ab1fb2f32744
Fabrice Payet <hello@fabricepayet.fr>
2020-10-18 10:24:58 +0400

    Add propertydata for simulator

--- models/Simulation.js
+++ models/Simulation.js
@@ -3,115 +3,115 @@ import mongoose from "mongoose";
 var schema = new mongoose.Schema(
   {
     userId: {
-      type: String,
+      type: String
     },
     title: {
-      type: String,
+      type: String
     },
     propertyPrice: {
       type: Number,
-      default: 0,
+      default: 0
     },
     agencyFees: {
       type: Number,
-      default: 0,
+      default: 0
     },
     visionRFees: {
       type: Number,
-      default: 0,
+      default: 0
     },
     notaryFees: {
       type: Number,
-      default: 0,
+      default: 0
     },
     additionalWorks: {
       type: Number,
-      default: 0,
+      default: 0
     },
     furnishing: {
       type: Number,
-      default: 0,
+      default: 0
     },
     bankDepositFees: {
       type: Number,
-      default: 0,
+      default: 0
     },
     bankingFees: {
       type: Number,
-      default: 0,
+      default: 0
     },
     contributionAmount: {
       type: Number,
-      default: 0,
+      default: 0
     },
     durationInMonths: {
       type: Number,
-      default: 0,
+      default: 0
     },
     creditRate: {
       type: Number,
-      default: 0,
+      default: 0
     },
     creditInsuranceRate: {
       type: Number,
-      default: 0,
+      default: 0
     },
     monthlyLoanPayment: {
       type: Number,
-      default: 0,
+      default: 0
     },
     monthlyRentalIncome: {
       type: Number,
-      default: 0,
+      default: 0
     },
     monthlyInsurancePayment: {
       type: Number,
-      default: 0,
+      default: 0
     },
     rentalGuaranteeInsurance: {
       type: Number,
-      default: 0,
+      default: 0
     },
     totalCoOwnershipCharges: {
       type: Number,
-      default: 0,
+      default: 0
     },
     propertyTax: {
       type: Number,
-      default: 0,
+      default: 0
     },
     PNOInsurance: {
       type: Number,
-      default: 0,
+      default: 0
     },
     approvedManagementCenter: {
       type: Number,
-      default: 0,
+      default: 0
     },
     accounting: {
       type: Number,
-      default: 0,
+      default: 0
     },
     electricity: {
       type: Number,
-      default: 0,
+      default: 0
     },
     water: {
       type: Number,
-      default: 0,
+      default: 0
     },
     internet: {
       type: Number,
-      default: 0,
+      default: 0
     },
     others: {
       type: Number,
-      default: 0,
-    },
+      default: 0
+    }
   },
   {
     timestamps: true,
-    collection: "simulations",
+    collection: "simulations"
   }
 );
 
