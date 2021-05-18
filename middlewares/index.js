8513b34fa3d6d8ac5c04a197544a2e380b846657
Alex <alex@alex-ThinkPad-T470s.(none)>
2021-05-18 15:55:44 +0400

    erase version with conflict merge

--- middlewares/index.js
+++
@@ -1,4 +0,0 @@
-export { default as checkRoles } from "./checkRoles";
-export { default as checkSuperAdmin } from "./checkSuperAdmin";
-export { default as errorHandle } from "./errorHandle";
-export { default as checkAccountDesactivated } from "./checkAccountDeactivated";
