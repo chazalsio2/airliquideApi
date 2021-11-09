d0499da50b3af697a579c9c151e1a3d2b74c1cb5
chazal <webmaster@vision-r.re>
2021-11-09 19:28:21 +0400

    dossier notaire

--- models/DossierNotaire.js
+++ models/DossierNotaire.js
@@ -2,9 +2,39 @@ import mongoose from "mongoose";
 
 var schema = new mongoose.Schema(
   {
-    contactId: {
-      type: mongoose.Types.ObjectId
-    }
+        societe:{
+          type: String
+        },
+        vendeur:{
+          type: String
+        },
+        adresse:{
+          type: String
+        },
+        Mail:{
+          type: String
+        },
+        phone:{
+          type: String
+        },
+        date_lieu:{
+          type: String
+        },
+        cp_ville:{
+          type: String
+        },
+        nationalite:{
+          type: String
+        },
+        profession:{
+          type: String
+        },
+        regime_matrimonial:{
+          type: String
+        },
+        contactId: {
+          type: mongoose.Types.ObjectId
+        }
   },
   
   {
