c8d0ca4e9e3574aacddff8434585ddfcd53c0028
Naifoudine <nmoualim@gmail.com>
2021-12-01 21:38:41 +0400

    Modification models Property.js

--- models/Property.js
+++ models/Property.js
@@ -250,7 +250,8 @@ schema.pre("save", async function (next) {
     const propertiesCount = await mongoose.models["Document"].countDocuments();
     const refTemps = `00000000${propertiesCount}`;
     this.ref = `${refTemps.substring(propertiesCount.toString().length)}`;
-    this.name = `${getPropertyType(this.type) || ""} ${this.livingArea} m² ${this.city || ""} ${this.landArea} m²`;
+    //this.name = `${getPropertyType(this.type) || ""} ${this.livingArea} m² ${this.city || ""} ${this.landArea} m²`;
+    this.name = `${getPropertyType(this.type) || ""} ${this.livingArea ? this.livingArea+" m²" : ""}  ${this.city || ""} ${this.landArea ? this.landArea+ " m²" : ""}`;
     next();
   } catch (e) {
     next(e);
