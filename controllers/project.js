7c4c539ca37d26b63b1d757dcc6d3909f3b7e4fd
chazal <webmaster@vision-r.re>
2022-06-14 19:31:31 +0400

    bon de vsisti auto

--- controllers/project.js
+++ controllers/project.js
@@ -2063,14 +2063,14 @@ export async function addDocumentToProject(req, res, next) {
       visibility
     }).save();
 
-    // const location = await uploadFile(
-    //   `project__${projectId}/${document._id}_${document.name}`,
-    //   fileData,
-    //   contentType
-    // );
+    const location = await uploadFile(
+      `project__${projectId}/${document._id}_${document.name}`,
+      fileData,
+      contentType
+    );
     await Document.updateOne(
       { _id: document._id },
-      { $set: { url: fileData } }
+      { $set: { url: location } }
     ).exec();
 
     await sendNewDocWebhook(document._id)
@@ -2111,14 +2111,14 @@ export async function addDocumentToProjectByExtrenPlatform(req, res, next) {
       visibility
     }).save();
 
-    const location = await uploadFile(
-      `project__${projectId}/${document._id}_${document.name}`,
-      fileData,
-      contentType
-    );
+    // const location = await uploadFile(
+    //   `project__${projectId}/${document._id}_${document.name}`,
+    //   fileData,
+    //   contentType
+    // );
     await Document.updateOne(
       { _id: document._id },
-      { $set: { url: location } }
+      { $set: { url: fileData } }
     ).exec();
 
     
